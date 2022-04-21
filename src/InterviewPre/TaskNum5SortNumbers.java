b5c1a21002b7d7b9693277cfe5c9dbfdb8d9f38f
mrefi <Mrefiyev84@gmail.com>
2022-04-20 22:48:47 -0400

    Initial commit

---
+++ src/InterviewPre/TaskNum5SortNumbers.java
@@ -0,0 +1,19 @@
+package InterviewPre;
+
+public class TaskNum5SortNumbers {
+    /*5.
+Don't use sort method
+ArrayList<Integer> list =  new ArrayList<>(Arrays.asList(1,10, 50, 2, 4, 50, 10, 3, 2,5, 4));
+first
+output
+[1, 2, 2, 3, 4, 4, 5, 10, 10, 50, 50]
+
+second
+[50, 50, 10, 10, 5, 4, 4, 3, 2, 2, 1]
+
+---------------SECOND------------
+Store into Array
+2int[] arr = {1, 2, 3, 49, 6, 5};
+ArrayList<Integer> list = new ArrayList(); */
+
+}
